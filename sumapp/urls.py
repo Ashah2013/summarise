355c697c148ba015b4dbdcf9d711bcd0b6197733
Ashah2013 <63425649+Ashah2013@users.noreply.github.com>
2021-01-07 02:44:42 +0530

    Update urls.py

--- sumapp/urls.py
+++ sumapp/urls.py
@@ -2,8 +2,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
 from django.views.static import serve
 from django.conf.urls import url
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
