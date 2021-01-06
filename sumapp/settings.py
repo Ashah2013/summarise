59cacf2e193bad462ca98880eccc28a6ebefd31f
Ashah2013 <63425649+Ashah2013@users.noreply.github.com>
2021-01-07 02:08:38 +0530

    Update settings.py

--- sumapp/settings.py
+++ sumapp/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'u%f%6yj&jhgm_0)y^^f92%9hmt!*)5_(ml*!+8*ae+9gt6bw$w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
